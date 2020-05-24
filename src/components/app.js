c39fdb9de955de26083c34344c4d12aa9b75dbac
krish <gopannagarishiva@gmail.com>
2020-05-24 13:05:32 +0530

    create-react-app

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
-import Header from './header';
-import Container from './container';
 import './app.scss';
 
 export default () => {
-    return <div className="_app">
-        <Header />
-        <Container />
-    </div>
+    return <div className="_app">APP.JS</div>
 }
--- src/components/header/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.scss'
-
-
-export default ({ result }) => {
-    return <div className="header">
-    </div>
-}
